df32562c2398954171fa3b94284a807f98ad2e92
dtracydepp <tracydepp82@gmail.com>
2020-12-17 16:37:49 -0600

    changed header

--- src/button/SearchAllButton.js
+++ src/button/SearchAllButton.js
@@ -4,7 +4,7 @@ import "../button/SearchAllButton.css"
 export const SearchAll = (props) => {
     return (
         <>
-            <h2>crèche</h2>
+            
             <p>Hello, let us help you find the perfect child care provider!</p>
             <button onClick={() => props.history.push("/allproviders")}>
                 Search All Providers
